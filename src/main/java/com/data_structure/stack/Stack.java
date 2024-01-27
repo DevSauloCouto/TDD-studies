8682011d63440dda56644a8c0cc3c3465364162d
SauloCouto <saulocouto1221@gmail.com>
2024-01-27 01:38:33 -0300

    Solution for the case tests implemented

--- src/main/java/com/data_structure/stack/Stack.java
+++ src/main/java/com/data_structure/stack/Stack.java
@@ -1,25 +1,46 @@
 package com.data_structure.stack;
 
+import com.data_structure.stack.exceptions.StackException;
+
 public class Stack {
 
-    private String elements;
-    private Integer quantity = 0;
+    private String[] elements;
+    private Integer index = 0;
+
+    public Stack(int size) {
+        this.elements = new String[size];
+    }
 
     public Boolean isEmpty() {
-        return elements == null;
+        return index == 0;
     }
 
     public Integer size() {
-        return quantity;
+        return index;
     }
 
     public void stackUp(String element) {
-        this.elements = element;
-        quantity++;
+        if (index == elements.length) {
+            throw new StackException("Stack full: don't possible stack up element if the stack is full");
+        }
+
+        this.elements[index] = element;
+        index++;
     }
 
-    public String firstElement() {
-        return elements;
+    public String top() {
+        return elements[index - 1];
+    }
+
+    public String unstack() {
+        if (isEmpty()) {
+            throw new StackException("Stack empty: don't possible unstack element");
+        }
+        String topElement = top();
+
+        index--;
+
+        return topElement;
     }
 
 }
